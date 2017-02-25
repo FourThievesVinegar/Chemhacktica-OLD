20c08dc69a9734dc6aa429d66908cb8c4d44016b
DynamoOfVolition <cat.bernaciak@gmail.com>
2017-02-24 23:47:43 -0500

    wrote getpage postpage fxns, left off trying to scrape rxns

--- crawl.py
+++ crawl.py
@@ -1,51 +1,18 @@
 from lxml import html
-#from lxml import etree 
 import requests
 import re
-import random
-import time 
-import socket 
+#import random
+#import time 
+from nist_modules import postpage
+from nist_modules import getrxns
 
-# ============================================================================================
-def getpage(url,payload):
-
-   # define time delays 
-   delay = random.uniform(1,1.5)
-   sleepy_time = delay*19.19
-   
-   again=True
-   while(again):
-      try:
-         r = s.post(url, data = payload, timeout=(9,33.3))
-         if(r.status_code==200):
-            print('Status OK: ', r.status_code)
-            again=False
-         else:
-            print('Status not OK: ', r.status_code)        
-            print('sleeping for', sleepy_time, 'seconds.....')
-            time.sleep(sleepy_time)
-#      except socket.gaierror as e:
-#         print('error: ', e)
-#         print('sleeping for', sleepy_time, 'seconds.....')
-#         time.sleep(sleepy_time)
-#      except socket.timeout as e:
-#         print('error: ', e)
-#         print('sleeping for', sleepy_time, 'seconds.....')
-#         time.sleep(sleepy_time)
-      except Exception as e:
-         print('Exception: ', e)
-         print('sleeping for', sleepy_time, 'seconds.....')
-         time.sleep(sleepy_time)
-
-   return r
-
-# ============================================================================================
 # FILE INFO
 filename = "/home/indra/Documents/Projects/CRN_4TV/DATA/CASRN/result_files/checked/FOR_NIST_CRAWL/cas_data_1.txt"
 filehandle= open(filename, "r")
-#outfilename = "/home/indra/Documents/Projects/CRN_4TV/DATA/CASRN/result_files/cas_nist_1.txt"
-outfilename = "/home/indra/Documents/Projects/Chemhacktica/test_output.txt"
+outfilename = "/home/indra/Documents/Projects/Chemhacktica/cas_nist_1.txt"
 outhandle = open(outfilename, "w")
+anomfilename = "/home/indra/Documents/Projects/Chemhacktica/anom_output.txt"
+anomhandle = open(anomfilename, "w")
 
 # create session for connection pooling as we are hitting the same database repeatedly
 s = requests.Session()
@@ -56,29 +23,44 @@ url = 'http://kinetics.nist.gov/solution/SearchForm'
 for line in filehandle:
    print('===============================')
    fields = line.split('|')
-   rct1 = fields[0] 
-   print(rct1)
-   #rct1 = "100-21-0"
+#   rct1 = fields[0] 
+   rct1 = "100-00-5"
    payload = {"database":"solution",
               "REACTANT1":rct1, "REACTANT2":"", "REACTANT3":"", 
               "PRODUCT1":"", "PRODUCT2":"", "PRODUCT3":"", 
               "SOLVENT1":"", "SOLVENT2":"", "SOLVENT3":""}
 
-#   r = requests.post(url, data = payload)
-   r = getpage(url, payload)
+   r = postpage(s, url, payload)
    tree = html.fromstring(r.content)
    text = tree.xpath('//td[@valign="TOP"][@width="100%"]/text()')
    text2 = [el.replace('\n', '') for el in text]
    text3 = list(filter(None, text2))
-   text4 = re.findall('\d+', text3[0]) 
-
-   if(len(text4)!=0 and text4[0]!='0'):
-      print("There were", text4[0], "results.\n")
-      # here i could put a function to grab rxn details
+   try:
+      text4 = re.findall('\d+', text3[0]) 
+      if(len(text4)!=0 and text4[0]!='0'):
+         print("There were", text4[0], "results.\n")
+         rxn = getrxns(s, tree)
+         row = []
+         row = " | ".join([rct1, text4[0], fields[1], fields[2]])
+         outhandle.writelines(row+'\n')
+      elif(len(text4)==0):
+         print("There were", len(text4), "results.\n")
+   except IndexError:
+      print("Anomalous CASRN: ", rct1)
       row = []
-      row = " | ".join([rct1, text4[0], fields[1], fields[2]])
-      outhandle.writelines(row+'\n')
-   elif(len(text4)==0):
-      print("There were", len(text4), "results.\n")
+      row = " | ".join([rct1, fields[1], fields[2]])
+      anomhandle.writelines(row+'\n')
 
+   exit()
 outhandle.close()
+anomhandle.close()
+
+
+
+
+
+
+
+
+
+
---
+++ nist_modules.py
@@ -0,0 +1,82 @@
+from lxml import html
+import random
+import time 
+
+# ============================================================================================
+def postpage(s, url, payload):
+
+   # define time delays 
+   delay = random.uniform(1,1.5)
+   sleepy_time = delay*19.19
+
+   again=True
+   while(again):
+      try:
+         r = s.post(url, data = payload, timeout=(9,33.3))
+         if(r.status_code==200):
+            print('Status OK: ', r.status_code)
+            again=False
+         else:
+            print('Status not OK: ', r.status_code)        
+            print('sleeping for', sleepy_time, 'seconds.....')
+            time.sleep(sleepy_time)
+      except Exception as e:
+         print('Exception: ', e)
+         print('sleeping for', sleepy_time, 'seconds.....')
+         time.sleep(sleepy_time)
+
+   return r
+# ============================================================================================
+def getpage(s, url):
+
+   # define time delays 
+   delay = random.uniform(1,1.5)
+   sleepy_time = delay*19.19
+
+   again=True
+   while(again):
+      try:
+         r = s.get(url, timeout=(9,33.3))
+         if(r.status_code==200):
+            print('Status OK: ', r.status_code)
+            again=False
+         else:
+            print('Status not OK: ', r.status_code)        
+            print('sleeping for', sleepy_time, 'seconds.....')
+            time.sleep(sleepy_time)
+      except Exception as e:
+         print('Exception: ', e)
+         print('sleeping for', sleepy_time, 'seconds.....')
+         time.sleep(sleepy_time)
+
+   return r
+# ============================================================================================
+def getrxns(s, tree):
+
+   # get Details info
+   details = tree.xpath('//td/a[contains(@href,"Detail")]/@href')
+
+   # acess site using getpage
+   for d in details:
+      url = 'http://kinetics.nist.gov/solution/' +  d
+      r = getpage(s, url)
+      tree = html.fromstring(r.content)
+      # reactant details
+      # ... determine number of reactants
+      # ... ... count # of times 'Name' appears
+      # ... ... within each reactant block, test is 'CAS Number' is there, if not, give blank value
+      rct_block = tree.xpath('//font[text()="Reactant details"]/following-sibling::b/text()')
+#      rct_block = tree.xpath('//*[text()="Name"]')
+#      rct_block = tree.xpath('//b[text()="Reactant Details"]/following-sibling::b[text()="Name"][1]/preceding-sibling::b[text()="Product Details"]')
+      print(rct_block)
+
+      exit()     
+
+
+      # restructure data if necessary
+
+#   return rxns
+
+
+# ============================================================================================
+
