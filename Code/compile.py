3b514f96600763ecb2bfd457ae1648812c912fdf
DynamoOfVolition <cat.bernaciak@gmail.com>
2017-05-02 23:55:12 -0400

    finished compiling rxn results for reactant search

--- Code/compile.py
+++ Code/compile.py
@@ -5,37 +5,38 @@ rootdir = "/home/indra/Documents/Projects/CRN_4TV/DATA/RXN_RESULTS/"
 ID1 = ['O', 'W', 'R', 'F', 'V', 'X', 'S', 'E', 'N']
 ID2 = 'R'
 
-for i in range(0,1):
+# output file
+outfilename = rootdir + "nist_reactions.txt"
+outhandle = open(outfilename, "w")
+header = "|".join(["RXN_ID", "CASRN_RCT ", 
+                   "RCT1 ", "RCT2 ", "RCT3 ",
+                   "PRD1 ", "PRD2 ", "PRD3 ",
+                   "SLV1 ", "SLV2 ", "SLV3 "])
+outhandle.writelines(header+'\n')
+
+for i in range(0,9):
   
    # input file 
    infilename = rootdir +  "nist_reactions_cas_" + str(i+1) + ".txt"
    inhandle = open(infilename, "r")
 
-   # output file
-   outfilename = rootdir + "rxns_" + str(i+1) + ".txt"
-   outhandle = open(outfilename, "w")
-
-   # prep for new file
-   linecnt = -1  
-   header = "|".join(["RXN_ID", "CASRN_RCT ", 
-                  "RCT1 ", "RCT2 ", "RCT3 ",
-                  "PRD1 ", "PRD2 ", "PRD3 ",
-                  "SLV1 ", "SLV2 ", "SLV3 "])
-   outhandle.writelines(header+'\n')
+   linecnt = 0  
 
    for line in inhandle:
       fields = line.split('|')
 #      print(fields)
       linecnt += 1
-      if(linecnt > 0):
-         newID = ID1[i] + ID2 + str(linecnt)
+      row = []
+      if(linecnt > 1):
+         newID = ID1[i] + ID2 + str(linecnt-1)
          row = " | ".join([newID, fields[1], fields[2], 
                            fields[3], fields[4], fields[5], 
                            fields[6], fields[7], fields[8], 
                            fields[9], fields[10]])
 
-#         outhandle.writelines(row+'\n')
-         outhandle.writelines(row)
+#      outhandle.writelines(row+'\n')
+      outhandle.writelines(row)
+
 inhandle.close()
 outhandle.close()
 
