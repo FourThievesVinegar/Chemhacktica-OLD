c261ff647e02b8dd8c48b2f1510e50e6b7cb8ec5
DynamoOfVolition <cat.bernaciak@gmail.com>
2017-05-05 00:27:44 -0400

    first look at reactant frequency - power law as expected

--- Code/RXN_exploration.R
+++ Code/RXN_exploration.R
@@ -1,9 +1,27 @@
+library(ggplot2)
+
 # BRINGING IN RXN DATA
 filename = "/home/indra/Documents/Projects/CRN_4TV/DATA/RXN_RESULTS/nist_reactions.txt"
 df.data <- read.table(filename, header=TRUE, sep="|")
 
 # MOST FREQUENT REACTANTS
 casrn_count <- as.data.frame(table(df.data$CASRN_RCT))
+summary(casrn_count$Freq)
+plot(casrn_count$Freq)
 
 # DISTRIBUTION
 hist(df.data$CASRN_RCT)
+
+barplot(casrn_count[,2], names.arg=casrn_count[,1])
+
+
+ggplot(casrn_count, aex(x=reorder()))
+
+
+library("ggplot2")
+num <- casrn_count$Freq
+cat <- casrn_count$Var1
+data <- data.frame(num, cat)    
+ggplot(data,aes( x=reorder(cat,-num) ,num)   )+geom_bar(stat ="identity")
+
+x=reorder(cat,-num)
